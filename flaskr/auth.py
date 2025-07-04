72f6c8faf1b12e91fef3a2725b7c3ecf1e79ab04
Blackscure <wekesabuyahi@gmail.com>
2025-07-04 16:57:54 +0300

    modifications created

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -57,9 +57,8 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            # return redirect(url_for('index.html'))
-            return 'Home'
-        
+            return redirect(url_for('blog.index'))
+           
         flash(error)
 
     return render_template('auth/login.html')
@@ -79,7 +78,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return render_template('auth/login.html')
 
 
 def login_required(view):
